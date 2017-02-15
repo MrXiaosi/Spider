a5fc05ef7b2e72a9a5e7e70a2e8720045ef1b187
zmj <zhengmingjie@chinalof.com.cn>
2017-02-15 13:47:06 +0800

    增加粉丝递归爬取

--- person_sina_spider.py
+++ person_sina_spider.py
@@ -10,30 +10,71 @@ class PersonSinaSpider(Spider):
         'http://weibo.cn/1910440104/info'
     ]
 
+    catched_uid = ['1910440104']
+
     def start_requests(self):
-        yield Request(url='http://weibo.cn/1910440104/info', method='get', callback=self.parse, cookies=self.get_cookies())
+        yield Request(url='http://weibo.cn/1910440104/info', method='get', callback=self.catch_PcdPerosonInfo, cookies=self.get_cookies())
+        yield Request(url='http://weibo.cn/1910440104/follow', method='get', callback=self.catch_FollowUID, cookies=self.get_cookies())
 
-    def parse(self, response):
+        return
+
+    def catch_PcdPerosonInfo(self, response):
         sel = Selector(response)
-        c_div = sel.xpath('//div[@class="c"]/text()')
+        print "catch_PcdPerosonInfo DEBUG INFO:"
 
-        print "MY DEBUG INFO:"
+        c_div = sel.xpath('//div[@class="c"]/text()')
+        f = open('PCD_info.txt', 'a+')
         for each in c_div:
             tmp = each.extract().encode("gb18030")
             if unicode("昵称", "utf-8").encode("gb18030") == tmp[:4]:
                 print tmp
+                f.write(tmp + '\n')
             elif unicode("认证", "utf-8").encode("gb18030") == tmp[:4]:
                 print tmp
+                f.write(tmp + '\n')
             elif unicode("性别", "utf-8").encode("gb18030") == tmp[:4]:
                 print tmp
+                f.write(tmp + '\n')
             elif unicode("地区", "utf-8").encode("gb18030") == tmp[:4]:
                 print tmp
+                f.write(tmp + '\n')
             elif unicode("认证信息", "utf-8").encode("gb18030") == tmp[:4]:
                 print tmp
+                f.write(tmp + '\n')
             elif unicode("简介", "utf-8").encode("gb18030") == tmp[:4]:
                 print tmp
+                f.write(tmp + '\n')
             else:
                 continue
+
+
+        f.write('\n')
+        return
+
+    def catch_FollowUID(self, response):
+        sel = Selector(response)
+        print "catch_FollowUID DEBUG INFO:"
+
+        total_page = sel.xpath('//input[@name="mp"][@type="hidden"]/@value')
+        print int(total_page[0].extract())
+
+        tables = sel.xpath('//table')
+        for each_table in tables:
+            link = each_table.xpath('tr/td[1]/a/@href')
+            name = each_table.xpath('tr/td[2]/a[1]/text()')
+            print name[0].extract().encode("gb18030"), link[0].extract()
+
+            new_person = link[0].extract()
+            if 'u' == new_person[16]:
+                new_person = new_person[:16] + new_person[18:]
+
+            uid = new_person[18:]
+            print uid
+            # yield Request(url=new_person+'/info', method='get', callback=self.catch_PcdPerosonInfo, cookies=self.get_cookies())
+            # yield Request(url=new_person+'/follow', method='get', callback=self.catch_FollowUID, cookies=self.get_cookies())
+
+        return
+
 
     def get_cookies(self):
         '''
@@ -55,4 +96,5 @@ class PersonSinaSpider(Spider):
             'SUHB' : '0u5gtC2w_cCbQJ',
             'ALF' : '1518590335'
         }
+
         return cookies
