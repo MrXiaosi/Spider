d81440b30a90e9d7207d05b4f64d6b3e7e48d018
MrXiaosi <zmj1128@foxmail.com>
2017-02-28 16:19:54 +0800

    Update person_sina_spider.py
    个人信息页面解析

--- person_sina_spider.py
+++ person_sina_spider.py
@@ -1,100 +1,162 @@
 # coding:utf-8
-from scrapy.spider import Spider
-from scrapy.selector import Selector
-from scrapy.http import Request
 
-class PersonSinaSpider(Spider):
-    name = 'person_sina'
-    allowed_domains = ['weibo.cn']
-    start_urls = [
-        'http://weibo.cn/1910440104/info'
-    ]
+import sys
+import urllib
+import urllib2
+import cookielib
+import base64
+import re
+import json
+import rsa
+import binascii
+import logging
+#　import requests
+#　from bs4 import BeautifulSoup
 
-    catched_uid = ['1910440104']
+#　新浪微博的模拟登陆
+class weiboLogin:
+    def enableCookies(self):
+        #　获取一个保存cookies的对象
+        cj = cookielib.CookieJar()
+        #　将一个保存cookies对象和一个HTTP的cookie的处理器绑定
+        cookie_support = urllib2.HTTPCookieProcessor(cj)
+        #　创建一个opener,设置一个handler用于处理http的url打开
+        opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)
+        #　安装opener，此后调用urlopen()时会使用安装过的opener对象
+        urllib2.install_opener(opener)
 
-    def start_requests(self):
-        yield Request(url='http://weibo.cn/1910440104/info', method='get', callback=self.catch_PcdPerosonInfo, cookies=self.get_cookies())
-        yield Request(url='http://weibo.cn/1910440104/follow', method='get', callback=self.catch_FollowUID, cookies=self.get_cookies())
+    #　预登陆获得 servertime, nonce, pubkey, rsakv
+    def getServerData(self):
+        url = 'http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=ZW5nbGFuZHNldSU0MDE2My5jb20%3D&rsakt=mod&checkpin=1&client=ssologin.js(v1.4.18)&_=1442991685270'
+        data = urllib2.urlopen(url).read()
+        p = re.compile('\((.*)\)')
+        try:
+                json_data = p.search(data).group(1)
+                data = json.loads(json_data)
+                servertime = str(data['servertime'])
+                nonce = data['nonce']
+                pubkey = data['pubkey']
+                rsakv = data['rsakv']
+                return servertime, nonce, pubkey, rsakv
+        except:
+                print 'Get severtime error!'
+                return None
+                
+    #　获取加密的密码
+    def getPassword(self, password, servertime, nonce, pubkey):
+        rsaPublickey = int(pubkey, 16)
+        key = rsa.PublicKey(rsaPublickey, 65537)            #　创建公钥
+        message = str(servertime) + '\t' + str(nonce) + '\n' + str(password) #　拼接明文js加密文件中得到
+        passwd = rsa.encrypt(message, key)                  #　加密
+        passwd = binascii.b2a_hex(passwd)                   #　将加密信息转换为16进制。
+        return passwd
 
-        return
+    #　获取加密的用户名
+    def getUsername(self, username):
+        username_ = urllib.quote(username)
+        username = base64.encodestring(username_)[:-1]
+        return username
 
-    def catch_PcdPerosonInfo(self, response):
-        sel = Selector(response)
-        print "catch_PcdPerosonInfo DEBUG INFO:"
+     #　获取需要提交的表单数据   
+    def getFormData(self,userName,password,servertime,nonce,pubkey,rsakv):
+        userName = self.getUsername(userName)
+        psw = self.getPassword(password,servertime,nonce,pubkey)
+        
+        form_data = {
+            'entry':'weibo',
+            'gateway':'1',
+            'from':'',
+            'savestate':'7',
+            'useticket':'1',
+            'pagerefer':'http://weibo.com/p/1005052679342531/home?from=page_100505&mod=TAB&pids=plc_main',
+            'vsnf':'1',
+            'su':userName,
+            'service':'miniblog',
+            'servertime':servertime,
+            'nonce':nonce,
+            'pwencode':'rsa2',
+            'rsakv':rsakv,
+            'sp':psw,
+            'sr':'1366*768',
+            'encoding':'UTF-8',
+            'prelt':'115',
+            'url':'http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
+            'returntype':'META'
+            }
+        formData = urllib.urlencode(form_data)
+        return formData
 
-        c_div = sel.xpath('//div[@class="c"]/text()')
-        f = open('PCD_info.txt', 'a+')
-        for each in c_div:
-            tmp = each.extract().encode("gb18030")
-            if unicode("昵称", "utf-8").encode("gb18030") == tmp[:4]:
-                print tmp
-                f.write(tmp + '\n')
-            elif unicode("认证", "utf-8").encode("gb18030") == tmp[:4]:
-                print tmp
-                f.write(tmp + '\n')
-            elif unicode("性别", "utf-8").encode("gb18030") == tmp[:4]:
-                print tmp
-                f.write(tmp + '\n')
-            elif unicode("地区", "utf-8").encode("gb18030") == tmp[:4]:
-                print tmp
-                f.write(tmp + '\n')
-            elif unicode("认证信息", "utf-8").encode("gb18030") == tmp[:4]:
-                print tmp
-                f.write(tmp + '\n')
-            elif unicode("简介", "utf-8").encode("gb18030") == tmp[:4]:
-                print tmp
-                f.write(tmp + '\n')
-            else:
-                continue
+    #　登陆函数
+    def login(self,username,psw):
+        self.enableCookies()
+        url = 'http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18)'
+        servertime,nonce,pubkey,rsakv = self.getServerData()
+        formData = self.getFormData(username,psw,servertime,nonce,pubkey,rsakv)
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'}
+        req  = urllib2.Request(
+                url = url,
+                data = formData,
+                headers = headers
+        )
+        result = urllib2.urlopen(req)
+        text = result.read()
 
+        #　重定位网址，包含在脚本中，获取到之后才能真正地登陆
+        p = re.compile('location\.replace\([\'"](.*?)[\'"]\)')
+        try:
+                login_url = p.search(text).group(1)
+                #由于之前的绑定，cookies信息会直接写入
+                urllib2.urlopen(login_url)
+                logging.info("Login success!")
+        except:
+                logging.info('Login error!')
+                return 0
 
-        f.write('\n')
-        return
+def catch_person_info(url):
+    #　访问主页，把主页写入到文件中
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    text = response.read()
 
-    def catch_FollowUID(self, response):
-        sel = Selector(response)
-        print "catch_FollowUID DEBUG INFO:"
+    p = re.compile("<script>FM.view.*</script>")
+    match = p.findall(text)
+    for eachFm in match:
+        if "Pl_Official_PersonalInfo__58" in eachFm:
+            index = eachFm.find("html")
+            eachFm = eachFm[index+9:-12]
+            eachFm = js_fmview2html(eachFm)
+            save_file("tmp.html", eachFm)
 
-        total_page = sel.xpath('//input[@name="mp"][@type="hidden"]/@value')
-        print int(total_page[0].extract())
+def js_fmview2html(text):
+    '''
+        网页中<script>FM.view内代码转换为HTML
+    '''
+    text = text.replace("\\t", "")
+    text = text.replace("\\n", "")
+    text = text.replace("\\r", "")
+    text = text.replace("\\/", "")
 
-        tables = sel.xpath('//table')
-        for each_table in tables:
-            link = each_table.xpath('tr/td[1]/a/@href')
-            name = each_table.xpath('tr/td[2]/a[1]/text()')
-            print name[0].extract().encode("gb18030"), link[0].extract()
+    return text
 
-            new_person = link[0].extract()
-            if 'u' == new_person[16]:
-                new_person = new_person[:16] + new_person[18:]
+def save_file(filename, text):
+    '''
+        text保存到文件
+    '''
+    fp = open(filename, "wb+")
+    fp.write(text)
+    fp.close()
 
-            uid = new_person[18:]
-            print uid
-            # yield Request(url=new_person+'/info', method='get', callback=self.catch_PcdPerosonInfo, cookies=self.get_cookies())
-            # yield Request(url=new_person+'/follow', method='get', callback=self.catch_FollowUID, cookies=self.get_cookies())
+if __name__ == "__main__":
+    #　日志配置
+    logging.basicConfig(format='%(asctime)s  %(levelname)s : %(message)s', level=logging.INFO)
 
-        return
+    wblogin = weiboLogin()
+    print 'sina login...'
+    # username = raw_input(u'username:')
+    username = "13459400403"
+    # password = raw_input(u'password:')
+    password = "463698777"
+    wblogin.login(username,password)
 
-
-    def get_cookies(self):
-        '''
-            生成登陆cookies
-        '''
-        cookies = {
-            'login_sid_t' : 'c3c7941bf3900fccce90ed0494572212',
-            '_s_tentry' : '-',
-            'Apache' : '7729941129218.787.1487050772128',
-            'ULV' : '1487050772180:1:1:1:7729941129218.787.1487050772128:',
-            'SINAGLOBAL' : '7729941129218.787.1487050772128',
-            'UOR' : 'widget.weibo.com,login.sina.com.cn',
-            'SSOLoginState' : '1487054336',
-            'un' : '13459400403',
-            'wvr' : '6',
-            'SCF' : 'AlwBknm4fJ8w4AXTRXORwp-x7jy6yDR5sXglOalrNraLy_Hqu82FrxYndSCXU3mwqe7r4w3pDX3XHV6jWu0Io0Q.',
-            'SUB' : '_2A251ptj1DeRxGeRO41UX8ivPyD-IHXVW0k09rDV8PUJbmtAKLVDukW8Ernbo3gudai2sftCIVkN2GS832Q..',
-            'SUBP' : '0033WrSXqPxfM725Ws9jqgMF55529P9D9WWy-me7T3SdgWJKurW_V.k.5JpX5o2p5NHD95QEehnNSozfe0e0Ws4DqcjHi--fi-z7iKysi--fiKLFiKLFeKnp',
-            'SUHB' : '0u5gtC2w_cCbQJ',
-            'ALF' : '1518590335'
-        }
-
-        return cookies
+    url = 'http://weibo.com/p/1005051910440104/info'
+    catch_person_info(url)
